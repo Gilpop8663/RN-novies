4474543baefd059b714464dc3273a4c86b5fd1ea
GilPOP <wolfye@naver.com>
2022-01-05 07:18:08 +0900

    stack update

--- navigation/Stack.tsx
+++ navigation/Stack.tsx
@@ -1,12 +1,31 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Text, TouchableOpacity } from "react-native";
 
 const NativeStack = createNativeStackNavigator();
+
+const screenOne = ({ navigation: { navigate } }) => (
+  <TouchableOpacity onPress={() => navigate("Two")}>
+    <Text>go to two</Text>
+  </TouchableOpacity>
+);
+const screenTwo = ({ navigation: { navigate } }) => (
+  <TouchableOpacity onPress={() => navigate("Three")}>
+    <Text>go to three</Text>
+  </TouchableOpacity>
+);
+const screenThree = ({ navigation: { setOptions } }) => (
+  <TouchableOpacity onPress={() => setOptions({ title: "Hello" })}>
+    <Text>change title</Text>
+  </TouchableOpacity>
+);
 
 const Stack = () => {
   return (
     <NativeStack.Navigator>
-      <></>
+      <NativeStack.Screen name="One" component={screenOne} />
+      <NativeStack.Screen name="Two" component={screenTwo} />
+      <NativeStack.Screen name="Three" component={screenThree} />
     </NativeStack.Navigator>
   );
 };
