48de5c5d252c61892b87398d381ca57843f51049
GilPOP <wolfye@naver.com>
2022-01-06 00:42:09 +0900

    styled update

--- navigation/Stack.tsx
+++ navigation/Stack.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Text, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
 
 const NativeStack = createNativeStackNavigator();
 
